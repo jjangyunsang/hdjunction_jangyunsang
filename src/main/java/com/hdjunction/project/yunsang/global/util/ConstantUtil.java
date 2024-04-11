874bc46550e79c2a6d5e996dff0ba7670a7a3d02
yunsang <xggames@naver.com>
2024-04-11 15:09:38 +0900

    개선사항 반영
    1] PatientSearchRequestDto - 불변객체 전환 / ModelAttribute Default Value 세팅방식 변경
        (ModelAttribute는 적절한 생성자 혹은 Setter가 있으면 데이터매핑함)
    
    2] ResponseBodyAdvice 변경
        (controller에서 DTO외에 다른 리턴타입(String 등)인 경우 오류나는 부분 변경)
        -> ResponseBodyAdvice는 Controller실행이 끝나고 직렬화직전에 실행되는데, 직렬화할 Converter가 결정되었고, 여기서 다른타입이 들어오면 오류가나기떄문에 분기가 필요.
        -> ControllerAdvice basePackage로 범위를 잘 지정해주고 사용해야할 것이다.
    
    3] 클래스의 정적맴버변수 생성자 못만들도록 변경 (StringUtil)
    
    4] DataSource master/slave 분기처리

--- src/main/java/com/hdjunction/project/yunsang/global/util/ConstantUtil.java
+++ src/main/java/com/hdjunction/project/yunsang/global/util/ConstantUtil.java
@@ -4,8 +4,12 @@ package com.hdjunction.project.yunsang.global.util;
  * 매직리터럴 -> 상수 관리 Class
  */
 public class ConstantUtil {
-    public static String PERCENT = "%";
-    public static String DASH = "-";
+    public static final String MASTER = "master";
+    public static final String SLAVE = "slave";
+    public static final String PERCENT = "%";
+    public static final String DASH = "-";
+    public static final String SCHEMA_LOCATIONS = "/sql/create_schema.sql";
+    public static final String DATA_LOCATIONS = "/sql/insert_data.sql";
 
     private ConstantUtil() {
         throw new IllegalStateException("Utility class.");
